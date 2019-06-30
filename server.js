850f2669215b82cf8ffb09fa15777c0d8c3171df
Timotheeee <timothe_laborie@yahoo.fr>
2019-06-30 21:15:55 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,78 @@
+// Using express: http://expressjs.com/
+var express = require('express');
+// Create the app
+var app = express();
+
+// Set up the server
+// process.env.PORT is related to deploying on heroku
+var server = app.listen(process.env.PORT || 3000, listen);
+
+// This call back just tells us that the server has started
+function listen() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://' + host + ':' + port);
+}
+
+app.use(express.static('public'));
+
+var players = 0;
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io')(server);
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+        // We are given a websocket object in our function
+                function (socket) {
+                    players++;
+                    console.log("We have a new client: " + socket.id + "\nplayers: " + players);
+                    if (players === 2)
+                        io.sockets.emit('start', {});
+                    // When this user emits, client side: socket.emit('otherevent',some data);
+                    socket.on('dmg',
+                            function (data) {
+                                // Data comes in as whatever was sent, including objects
+                                console.log("Received: " + data.move);
+
+                                //Send it to all other clients
+                                socket.broadcast.emit('dmg', data);
+
+                                // This is a way to send to everyone including sender
+                                // io.sockets.emit('msg', data);
+
+                            }
+                    );
+                    socket.on('shield',
+                            function (data) {
+                                // Data comes in as whatever was sent, including objects
+                                console.log("Received shield");
+
+                                //Send it to all other clients
+                                socket.broadcast.emit('shield', {});
+
+                                // This is a way to send to everyone including sender
+                                // io.sockets.emit('msg', data);
+
+                            }
+                    );
+
+                    socket.on('end',
+                            function (data) {
+                                // Data comes in as whatever was sent, including objects
+                                console.log("Received end");
+
+                                players = 0;
+
+
+                            }
+                    );
+
+                    socket.on('disconnect', function () {
+                        console.log("Client has disconnected");//
+                        players--;
+                        if(players < 0)players =0;
+                    });
+                }
+        );
